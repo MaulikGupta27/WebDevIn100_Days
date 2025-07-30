1e321265461715a344cc30aed69955220367b43f
Maulik Gupta <guptamaulik27@gmail.com>
2025-07-30 20:24:51 +0530

    Updated the js and css of age calc

--- public/Day-37_AgeCalculator/script.js
+++ public/Day-37_AgeCalculator/script.js
@@ -1,43 +1,40 @@
 function calculateAge() {
-    let dobString = document.getElementById("dob").value;
+    const dobString = document.getElementById("dob").value;
 
     if (!dobString) {
         document.getElementById("result").innerHTML = "Please enter a date of birth.";
         return;
     }
 
-    let dob = new Date(dobString);
-    let today = new Date();
+    const dob = new Date(dobString);
+    const today = new Date();
+
+    if (dob > today) {
+        document.getElementById("result").innerHTML = `Date of birth must be before ${today.toLocaleDateString()}.`;
+        return;
+    }
 
     let ageYear = today.getFullYear() - dob.getFullYear();
     let ageMonth = today.getMonth() - dob.getMonth();
     let ageDay = today.getDate() - dob.getDate();
 
-    console.log(ageYear);
-    console.log(ageMonth);
-    console.log(ageDay);
-    
+    if (ageDay < 0) {
+        ageMonth--;
+        const prevMonth = new Date(today.getFullYear(), today.getMonth(), 0);
+        ageDay += prevMonth.getDate();
+    }
+
     if (ageMonth < 0) {
         ageYear--;
         ageMonth += 12;
     }
 
-    if (ageDay < 0) {
-        ageMonth--;
-        let prevMonth = new Date(today.getFullYear(), today.getMonth() - 1, 0);
-        ageDay += prevMonth.getDate();
-    }
-
-    if (dob > today) {
-        document.getElementById("result").innerHTML = `Date of birth needs to be earlier than ${today.toLocaleDateString()}.`;
-        return;
-    } else if(ageYear < 1950) {
+    if (dob.getFullYear() < 1950) {
         document.getElementById("result").innerHTML = "You are too young to use this application.";
         return;
-    } else {
-        document.getElementById("result").innerHTML = `Your age is: ${ageYear} years, ${ageMonth} months, ${ageDay} days`;
-        return;
     }
+
+    document.getElementById("result").innerHTML = `Your age is: ${ageYear} years, ${ageMonth} months, ${ageDay} days.`;
 }
 
 document.getElementById("button").addEventListener("click", calculateAge);
